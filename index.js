3c96379bbce52fc18e68df42ea956317da088161
leedawn <2g_hee@naver.com>
2021-03-26 23:17:37 +0900

    Add hover effect for main logo

--- index.js
+++
@@ -1,7 +0,0 @@
-$(".hov-logo").mouseover(function() {
-    $(this).attr("src", $(this).data("animated"))
-}),
-$(".hov-logo").mouseout(function() {
-    $(this).attr("src", $(this).data("static"))
-});
-  
